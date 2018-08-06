64927bab06634c748cd8016c3ce42f268809c708
Damodar Shenoy <itsdamslife@gmail.com>
2018-08-06 07:56:58 +0530

    Basic UIView animate with delay, options and completion handlers, nested animations

---
+++ Todo/AnimationsViewController.swift
@@ -0,0 +1,80 @@
+//
+//  AnimationsViewController.swift
+//  Todo
+//
+//  Created by Damodar Shenoy on 06/08/18.
+//  Copyright © 2018 itscoderslife. All rights reserved.
+//
+
+import UIKit
+
+class AnimationsViewController: UIViewController {
+
+    @IBOutlet weak var checkboxContainer: UIStackView!
+    @IBOutlet weak var checkBox: CheckBox!
+    @IBOutlet weak var checkBoxLabel: UILabel!
+    @IBOutlet weak var cbContainerCenterConstraint: NSLayoutConstraint!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        checkBox.backgroundColor = .clear
+        checkBoxLabel.backgroundColor = .clear
+        view.backgroundColor = .yellow
+        checkBox.isSelected = true
+
+        checkBox.alpha = 0.0
+        checkBoxLabel.alpha = 0.0
+        checkboxContainer.alpha = 0.0
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        UIView.animate(withDuration: 1.0, animations: { [unowned self] in
+            self.checkboxContainer.alpha = 1.0
+        })
+
+        UIView.animate(withDuration: 1.0, delay: 2.0, options: [.curveEaseOut], animations: { [unowned self] in
+            self.checkBox.alpha = 1.0
+        }, completion: nil)
+
+        UIView.animate(withDuration: 1.0, delay: 3.0, options: [.curveEaseOut], animations: { [unowned self] in
+            self.checkBoxLabel.alpha = 1.0
+        }, completion: { [unowned self] finished in
+            guard finished else { return }
+            UIView.animate(withDuration: 1.0, animations: { [unowned self] in
+                self.view.backgroundColor = .white
+            }, completion: { [unowned self] _ in
+                UIView.animate(withDuration: 1.0, delay: 0.5, options: [.curveEaseOut], animations: { [unowned self] in
+                    var frame = self.checkboxContainer.frame
+                    frame.origin.y -= 50
+                    self.checkboxContainer.frame = frame
+                }, completion: nil)
+            })
+        })
+
+    }
+
+    @IBAction func checkboxTapped(_ sender: Any) {
+        let btn: UIButton = sender as! UIButton
+        btn.isSelected = !btn.isSelected
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
